3e35a9229472a12ad3abccfb8d3a8faa796cb6d7
David Garry <daithigarry@hotmail.com>
2022-07-16 09:39:25 +0000

    class Battleship

--- run.py
+++ run.py
@@ -1,3 +1,75 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class GameBoard:
+  def __init__(self, board):
+    self.board = board
+
+  def get_letters_to_numbers():
+    """ letters to numbers is used to identify the user column input to numbers on the grid"""
+    """ The letters to numbers method was taken from the work of "Knowledge Mavens" youtube channel """
+    letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5}
+    return letters_to_numbers
+
+    """ labels the columns on the board"""
+  def print_board(self):
+      print("  A B C D E F")
+      print("+ - - - - - - +")
+      row_number = 1
+
+      for row in range(6):
+        """ defines the loop that runs through the board and registers the numbers and columns and the surrounding formatting"""
+        self.board.append(['-'] * 6)
+        number = 0
+
+      for row in range(6):
+        print(str(number + 1), end = '|')
+        for column in range(len(self.board[number])):
+          print(self.board[number][column], end= ' ')
+        print('| ')  
+        number += 1
+      print("+ - - - - - - +")
+
+class Battleship:
+  def __init__(self, board):
+    self.board = board
+
+  def create_ships_on_board(self):
+    for i in range(6):
+      self.guess_row, self.guess_column = random.randint(0, 5), random.randint(0, 5)
+      while self.board[self.guess_row][self.guess_column] == "S":
+        self.guess_row, self.guess_column = random.randint(0, 5), random.randint(0, 5)
+      self.board[self.guess_row][self.guess_column] = "S"
+    return self.board
+
+  def get_user_input(self):
+    """ 
+        player input functions are created to make sure the player selects 
+        an existing row (x) and column (y) to insure the 
+        instructions to the program are valid.
+        """
+    try:
+      guess_row = input("Enter the row of the ship: e.g. 123 ")
+      while guess_row not in '123456':
+          print('Cannot place ship here, please select a row')
+          guess_row = input("Enter the row of the ship: e.g. 123 ")
+
+      guess_column = input("Enter the column of the ship: e.g. ABC ").upper()
+      while guess_column not in "ABCDEF":
+          print("Cannot place ship here, please select a column")
+          guess_column = input("Enter the column of the ship: e.g. ABC ").upper()
+      return int(guess_row) - 1, GameBoard.get_letters_to_numbers()[guess_column]
+    except ValueError and KeyError:
+      print("Input is not valid")
+      return self.get_user_input()
+
+  def count_ships_you_hit(self):
+    """ 
+        hit ships counter to start at 0, and will increase by 1 for each 
+        X (direct hit) that is found on the board.
+        """
+    ships_you_hit = 0
+    for row in self.board:
+      for column in row:
+        if column == "S":
+          ships_you_hit += 1
+    return ships_you_hit
